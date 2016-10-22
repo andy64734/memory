58558a9dcaad01d7802ae13f1d9f526663e2e78a
Andy Winston <andywinston@wi.rr.com>
2016-10-22 15:21:37 -0500

    Finished everything with the queuing.

--- memory.py
+++ memory.py
@@ -1,13 +1,14 @@
 """This is for the file with memory management."""
 from Queue import Queue
+from collections import deque
 from math import *
 def main():
     num_test_cases = input()
-    for test_case in range(0,3):
+    for test_case in range(0,num_test_cases):
         case_stats_line = raw_input()
-        num_pages = case_stats_line.split()[0]
-        page_size = case_stats_line.split()[1]
-        num_accesses = case_stats_line.split()[2]
+        num_pages = int(case_stats_line.split()[0])
+        page_size = int(case_stats_line.split()[1])
+        num_accesses = int(case_stats_line.split()[2])
 
         fifo_replacements = 0
         lru_replacements = 0
@@ -16,21 +17,31 @@ def main():
         for cycle in range(0,num_accesses):
             access_addr.append(input())
 
-        fifo = Queue(num_pages)
+        fifo = deque([],num_pages)
         for addr in access_addr:
-            if not fifo.full():
-                fifo.put(addr/page_size)
-            else:
-                curr_page = fifo.get()
-                if(addr/page_size!=curr_page):
+            page = addr/page_size
+            if page not in fifo:
+                if len(fifo)>=fifo.maxlen:
+                    fifo.popleft()#Remove the least recently added page.
                     fifo_replacements+=1
+                fifo.append(page)#Always add the page/add it back.
 
-        lru = []
+        lru = deque([],num_pages)
         for addr in access_addr:
-            pass
+            page = addr/page_size
+            if page in lru:
+                lru.remove(page)
+            else:
+                if len(lru)>=lru.maxlen:
+                    lru.popleft()# Removes least recently used.
+                    lru_replacements+=1
+            lru.append(page)#No matter what, page should be added to most recent place.
 
-
-    pass
+        if fifo_replacements>lru_replacements:
+            print("yes"),#Comma to prevent new line.
+        else:
+            print("no"),
+        print(str(fifo_replacements)+" "+str(lru_replacements))
 
 if __name__=="__main__":
     main()
